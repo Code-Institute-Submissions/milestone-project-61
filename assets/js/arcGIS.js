39ed61aa118f01f38559ee9383616cf4f2755021
mvmolloy <mvamolloy@gmail.com>
2020-05-18 09:58:55 +0000

    adding marker to map over HQ

--- assets/js/arcGIS.js
+++ assets/js/arcGIS.js
@@ -1,6 +1,6 @@
 //load esri modules 
 require(["esri/Map", "esri/WebScene","esri/views/SceneView","esri/widgets/BasemapToggle", "esri/widgets/BasemapGallery", "esri/Graphic", "esri/layers/GraphicsLayer", "esri/widgets/Sketch", "esri/symbols/PolygonSymbol3D","esri/symbols/ExtrudeSymbol3DLayer"],
-    function(Map, WebScene, SceneView, BasemapToggle, BasemapGallery, Graphic, GraphicsLayer, Sketch, PolygonSymbol3D, ExtrudeSymbol3DLayer){
+    function(Map, WebScene, SceneView, BasemapToggle, BasemapGallery, Graphic, GraphicsLayer, Sketch, PolygonSymbol3D, ExtrudeSymbol3DLayer, HQgraphic){
         /*mapinit*/
         var graphicsLayer = new GraphicsLayer({
             screenSizePerspectiveEnabled: true, //make points further away smaller, increase depth perception
@@ -22,8 +22,27 @@ require(["esri/Map", "esri/WebScene","esri/views/SceneView","esri/widgets/Basema
             tilt: 70  // perspective in degrees
             }
         });
-        map.add(graphicsLayer); 
+        map.add(graphicsLayer);
+    //Add marker over HQ
+        var pictureGraphic = new Graphic({
+            geometry: {
+                type: "point",
+                longitude: -3.340692,
+                latitude: 57.007759
+            },
+            symbol: {
+                type: "picture-marker",
+                url: "assets/img/marker-img/marker-hq.png",
+                width: "30px",
+                height: "30px", 
+                xoffset: 0,
+                yoffset: 30,
+            },
+            popupTemplate: {
+                title: "Cairngorms Outdoors HQ",
+                content: "Invercauld Road, Braemar, Aberdeenshire, AB35 5XR",
+            }
+        });
+        graphicsLayer.add(pictureGraphic);
     }
 )
-
-
