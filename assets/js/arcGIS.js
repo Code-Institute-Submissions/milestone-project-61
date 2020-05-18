d93efec1ba6d784e0dea6054038cc1b93462371c
mvmolloy <mvamolloy@gmail.com>
2020-05-18 13:45:06 +0000

    removing HQ graphish as esri map parameter

--- assets/js/arcGIS.js
+++ assets/js/arcGIS.js
@@ -1,6 +1,6 @@
 //load esri modules 
 require(["esri/Map", "esri/WebScene","esri/views/SceneView","esri/widgets/BasemapToggle", "esri/widgets/BasemapGallery", "esri/Graphic", "esri/layers/GraphicsLayer", "esri/widgets/Sketch", "esri/symbols/PolygonSymbol3D","esri/symbols/ExtrudeSymbol3DLayer"],
-    function(Map, WebScene, SceneView, BasemapToggle, BasemapGallery, Graphic, GraphicsLayer, Sketch, PolygonSymbol3D, ExtrudeSymbol3DLayer, HQgraphic){
+    function(Map, WebScene, SceneView, BasemapToggle, BasemapGallery, Graphic, GraphicsLayer, Sketch, PolygonSymbol3D, ExtrudeSymbol3DLayer){
         
         mapInit();
         HQgraphic();
