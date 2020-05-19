fb3fbb7daf779c20049707fd86286de90499c2e6
mvmolloy <mvamolloy@gmail.com>
2020-05-19 13:32:55 +0000

    add widget to map to toggle between satellite map and topovector

--- assets/js/arcGIS.js
+++ assets/js/arcGIS.js
@@ -26,11 +26,23 @@ require(["esri/Map", "esri/WebScene","esri/views/SceneView","esri/widgets/Basema
             }
         });
         map.add(graphicsLayer);
+        //add widget to toggle between topovector and img basemap 
+        var toggle = new BasemapToggle({
+            //set detault to view/topovector 
+            view: view,
+            //set second basemap to satellite/img
+            nextBasemap: "satellite"
+        });
+        view.ui.add(toggle, "bottom-left");
+   
+  //padding for the toggle widg around nav toggler- map stops working if not declared
 
-        function ConstructMarker(long, lat, markerType, popupTitle, popupContent, popupImg){
+
+        function ConstructMarker(long, lat, markerType, markerSize, popupTitle, popupContent, popupImg){
             this.long = long
             this.lat = lat
             this.markerType = markerType
+            this.markerSize = markerSize
             this.title = popupTitle
             this.content = popupContent 
             this.img = popupImg
@@ -45,8 +57,8 @@ require(["esri/Map", "esri/WebScene","esri/views/SceneView","esri/widgets/Basema
                 symbol: {
                     type: "picture-marker",
                     url: `assets/img/marker-img/marker-${markerObject.markerType}.png`,
-                    width: "30px",
-                    height: "30px", 
+                    width: `${markerObject.markerSize}px`,
+                    height: `${markerObject.markerSize}px`, 
                     xoffset: 0,
                     yoffset: 30,
                 },
@@ -57,16 +69,16 @@ require(["esri/Map", "esri/WebScene","esri/views/SceneView","esri/widgets/Basema
             })
             graphicsLayer.add(markerGraphic);
         }
-        hqMarker = new ConstructMarker (-3.340692, 57.007759, "hq", "Cairngorms Outdoors HQ", "Invercauld Road, Braemar, Aberdeenshire, AB35 5XR")
-        campMarker1 = new ConstructMarker (-3.668879, 57.070710, "camp", "Camp on the summit of Ben Macdui", "The highest peak in the Cairngorms")
-        campMarker2 = new ConstructMarker (-4.053160, 56.945606, "camp", "Camp on the beach of Loch an T-Seilich", "..")
-        waterMarker1 = new ConstructMarker (-3.674994, 57.020003, "water", "Paddle down the river Dee", "Take in this awesome landscape as the water carries you to your pickup point")
-        waterMarker2 = new ConstructMarker (-4.043326, 56.989550, "water", "Paddle down the wild River Tromie", "Take in this awesome landscape as the water carries towards Gaick Lodge")
-        waterMarker3 = new ConstructMarker (-4.248634, 56.927478, "water", "Travel 14 miles along Loch Ericht by power boat", "Arrive at your pickup point in style!")
-        pickupMarker1 = new ConstructMarker (-3.615609, 56.977300, "van", "Pickup Point: River Dee", "Drive the scenic route back to HQ")
-        pickupMarker2 = new ConstructMarker (-4.042070, 56.944850, "van", "Pickup Point: Gaick Lodge", "Drive the scenic route back to HQ")
-        pickupMarker3 = new ConstructMarker (-4.248634, 56.927478, "van", "Pickup Point: Loch Ericht", "Drive the scenic route back to HQ")
-        pickupMarkerWater1 = new ConstructMarker (-4.438109, 56.733385, "van", "Pickup Point: Loch Ericht", "Switch over from boat to landrover and drive the scenic route back to HQ")
+        hqMarker = new ConstructMarker (-3.340692, 57.007759, "hq", 30, "Cairngorms Outdoors HQ", "Invercauld Road, Braemar, Aberdeenshire, AB35 5XR")
+        campMarker1 = new ConstructMarker (-3.668879, 57.070710, "camp", 30, "Camp on the summit of Ben Macdui", "The highest peak in the Cairngorms")
+        campMarker2 = new ConstructMarker (-4.053160, 56.945606, "camp", 30, "Camp on the beach of Loch an T-Seilich", "..")
+        waterMarker1 = new ConstructMarker (-3.674994, 57.020003, "water", 30, "Paddle down the river Dee", "Take in this awesome landscape as the water carries you to your pickup point")
+        waterMarker2 = new ConstructMarker (-4.043326, 56.989550, "water", 30, "Paddle down the wild River Tromie", "Take in this awesome landscape as the water carries towards Gaick Lodge")
+        waterMarker3 = new ConstructMarker (-4.248634, 56.927478, "water", 30, "Travel 14 miles along Loch Ericht by power boat", "Arrive at your pickup point in style!")
+        pickupMarker1 = new ConstructMarker (-3.615609, 56.977300, "van", 30, "Pickup Point: River Dee", "Drive the scenic route back to HQ")
+        pickupMarker2 = new ConstructMarker (-4.042070, 56.944850, "van", 30, "Pickup Point: Gaick Lodge", "Drive the scenic route back to HQ")
+        pickupMarker3 = new ConstructMarker (-4.248634, 56.927478, "van", 30, "Pickup Point: Loch Ericht", "Drive the scenic route back to HQ")
+        pickupMarkerWater1 = new ConstructMarker (-4.438109, 56.733385, "van", 30, "Pickup Point: Loch Ericht", "Switch over from boat to landrover and drive the scenic route back to HQ")
 
         addMarker(hqMarker);
 
