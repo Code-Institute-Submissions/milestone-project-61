4c55a8f2d7fe8e75281346e4a5781eaebc43f30c
mvmolloy <mvamolloy@gmail.com>
2020-05-20 14:20:08 +0000

    adding previous/back/forward nav controls to about you form

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,41 +68,84 @@ $("#btn-about-us").click(function () {
         return false;
     })
 
-
-
     $("#btn-about-you").click(function(){
-        $(this).hide();
-        $("#clientType").removeClass("display-none");
+        $(this).hide(); 
+        $("#clientType").removeClass("display-none").addClass("form-active");
     })
-    
-    $("label").click(function () {
-        $(this).parent().parent().hide();
-        $(this).parent().parent().next().removeClass("display-none");
-        if($(this).parent().parent().next().is("#toMap")){
-            $("#about-you-back-text").html("back")
-        } 
-     });
 
     $('input:radio[name="clientType"]').change(function () {
-        $("#about-you-back").removeClass("display-none");
         if ($(this).val() == "individ") {
-            $("#groupType").hide();
-            $("#enterName").removeClass("display-none");
             $("#promptName").html("your name:");
             $("#message").val("I'm looking to organise an expedition. ");
-            $("#btn-about-form-next").click(function () {
-                $("#enterName").hide();
-                $("#duration").removeClass("display-none");
-            });
-        } 
-        else {
-            $("#about-us-client-img").attr("src", "assets/img/team-img/group-hex.png");
-            $("#btn-about-form-next").click(function () { 
-                $("#enterName").hide(); 
-                $("#groupNumber").removeClass("display-none");
-            }); 
+            $("#about-us-client-img").attr("src", "assets/img/team-img/individ-hex.png");
         }
+        else {
+           $("#about-us-client-img").attr("src", "assets/img/team-img/group-hex.png");
+        }       
     });
+
+    $("label, #btn-about-form-next").click(function(){
+        if($("#individ").is(":checked") || $(this).is("#individ-label")) {
+            if($(".form-active").next("div").hasClass("about-form-individ-panel")){
+                $(".form-active").next(".about-form-panel").removeClass("display-none").addClass("form-active");
+                $(".form-active").prev(".about-form-panel").addClass("display-none").removeClass("form-active");
+            }
+            else {
+                $(".form-active").nextUntil(".about-form-individ-panel").next().addClass("form-active").removeClass("display-none");
+                $(".form-active").prevUntil(".about-form-individ-panel").prev().addClass("display-none").removeClass("form-active");
+            }
+        }
+        else {
+            $(".form-active").next(".about-form-panel").removeClass("display-none").addClass("form-active");
+            $(".form-active").prev(".about-form-panel").addClass("display-none").removeClass("form-active");
+        }
+        if ($(".form-active").prev().is("#enterName")){
+            if ($("#group").is(":checked")) {
+                if ($("#school").is(":checked") || $("#corporate").is(":checked")) {
+                    $("#message").val(`I'm looking to arrange an expedition for a group from ${clientName.value}. `);
+                    $("#sizePrompt").html(`How many people are in your group from ${clientName.value}?`)
+                    $("#durationPrompt").html(`How many days would you like your group from ${clientName.value} to spend with us?`)
+                    $("#msgMap").html(`We've generated a 3D expedition for your group from ${clientName.value} based on your responses.`)
+                }  
+                else {
+                    $("#durationPrompt").html("How many days would you like your expedition to last?")
+                    $("#sizePrompt").html(`How many people are in your group?`)  
+                    $("#msgMap").html(`We've generated a 3D expedition for your group based on your responses.`) 
+                }
+            } 
+            else {
+                $("#custName").val(`${clientName.value}`); //set contact form name to name entered here
+                $("#durationPrompt").html(`Hi, ${clientName.value}! How many days would you like your expedition to last?`);
+                $("#msgMap").html(`${clientName.value}, we've generated a 3D expedition for you based on your responses.`)
+            } 
+        }
+        if($(".form-active").is("#toMap")){
+            $("#about-you-back-text").html("back");
+        }       
+            $("#about-you-back").removeClass("display-none");
+    })  
+
+    $("#about-you-back").click(function(){
+        if($("#individ").is(":checked")){ 
+            if($(".form-active").prev("div").hasClass("about-form-individ-panel")){
+                $(".form-active").prev(".about-form-panel").removeClass("display-none").addClass("form-active");
+                $(".form-active").next(".about-form-panel").addClass("display-none").removeClass("form-active");
+            }
+            else {
+                $(".form-active").prevUntil(".about-form-individ-panel").prev().addClass("form-active").removeClass("display-none");
+                $(".form-active").nextUntil(".about-form-individ-panel").next().addClass("display-none").removeClass("form-active");
+            }
+        }
+        else {
+            $(".form-active").prev(".about-form-panel").removeClass("display-none").addClass("form-active");
+            $(".form-active").next(".about-form-panel").addClass("display-none").removeClass("form-active");
+        }
+        if ($(".form-active").is("#clientType")){
+            $(this).addClass("display-none");
+        } 
+        $("#about-you-back-text").html("previous");
+              
+    }) 
 
     $('input:radio[name="groupType"]').change(function () {
         if ($(this).val() == "friends") {
@@ -119,26 +162,15 @@ $("#btn-about-us").click(function () {
         $("#about-client-name").html(`${clientName.value}`);
         if ($("#group").is(":checked")) {
             if ($("#school").is(":checked") || $("#corporate").is(":checked")) {
-                $("#message").val(`I'm looking to arrange an expedition for a group from ${clientName.value}. `);
-                $("#sizePrompt").html(`How many people are in your group from ${clientName.value}?`)
-                $("#durationPrompt").html(`How many days would you like your group from ${clientName.value} to spend with us?`)
-                $("#msgMap").html(`We've generated a 3D expedition for your group from ${clientName.value} based on your responses.`)
-            }  
+                 $("#message").val(`I'm looking to arrange an expedition for a group from ${clientName.value}. `);
+            }
             else {
                 $("#message").val("We're a group of friends/family looking to arrange an expedition. ");
-                $("#durationPrompt").html("How many days would you like your expedition to last?")
-                $("#sizePrompt").html(`How many people are in your group?`)  
-                $("#msgMap").html(`We've generated a 3D expedition for your group based on your responses.`) 
             }
-        } 
-        else {
-            $("#custName").val(`${clientName.value}`); //set contact form name to name entered here
-            $("#durationPrompt").html(`Hi, ${clientName.value}! How many days would you like your expedition to last?`);
-            $("#msgMap").html(`${clientName.value}, we've generated a 3D expedition for you based on your responses.`)
-        } 
+        }
     });
 
-    $("#groupNumber").change(function () {
+    $('input:radio[name="groupSize"]').change(function () {
         if ($("#smGroup").is(":checked")) {
             $("#message").val($("#message").val() + "There's between 2 and 5 of us ");
         } 
@@ -150,30 +182,31 @@ $("#btn-about-us").click(function () {
         }
     });
 
-    $("#duration").change(function () {
+    $('input:radio[name="duration"]').change(function () {
         if ($("#group").is(":checked")) {
             if ($("#days1").is(":checked")) {
-                $("#message").val($("#message").val() +"and we're looking to plan an expedition just for the day.");
+                $("#message").val($("#message").val() + "and we're looking to plan an expedition just for the day.");
             } 
             else if ($("#days2").is(":checked")) {
-                $("#message").val($("#message").val() +"and we're looking to plan an expedition for two days.");
+                $("#message").val($("#message").val() + "and we're looking to plan an expedition for two days.");
             } 
             else {
-                $("#message").val($("#message").val() +" and we're looking to plan an expedition for three days or more.");
+                $("#message").val($("#message").val() + "and we're looking to plan an expedition for three days or more.");
             }
         } 
         else {
             if ($("#days1").is(":checked")) {
-                $("#message").val($("#message").val() + "I'm looking to plan an expedition, just for the day.");
+                $("#message").val($("#message").val() + "I'm looking for it to be just for the day.");
             } 
             else if ($("#days2").is(":checked")) {
-                $("#message").val($("#message").val() +"I'm looking to plan an expedition for two days.");
+                $("#message").val($("#message").val() +"I'm looking for it it to last for two days.");
             } 
             else {
-                $("#message").val($("#message").val() +"I'm looking to plan an expedition for three days or more.");
+                $("#message").val($("#message").val() +"I'm looking for it to last for three days or more.");
             }
         }     
     });
+
 
 //Map
     //checks if device is a touchscreen - if it is, displays scroll icon: check out https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
