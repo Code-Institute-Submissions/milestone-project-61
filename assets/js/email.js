733ae16dfbd07a84a477d4e681c740a9cad007c6
mvmolloy <mvamolloy@gmail.com>
2020-05-21 19:50:34 +0000

    adding some comments to jquery

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,18 +1,22 @@
 function submitForm(contactForm) {
     initEmailJS();
+    //pass user inputs to EmailJS
     emailjs.send("gmail", "cairngormsOutdoorsContact", {
         "custName": contactForm.custName.value,
         "custNumber": contactForm.custNumber.value,
         "custEmail": contactForm.custEmail.value,
         "custMsg": contactForm.custMsg.value
     }) 
+    //log an success/error message to console and display success/error feedback to user
     .then( 
         function(response) {
+            //form success
             console.log("FORM SENT", response);
             $("#emailJS-success").show();
             $("#form-container").hide();
         },   
         function(error) {
+            //form error
             console.log("ERROR: FORM NOT SENT", error);
             $("#emailJS-error").show();
             $("#form-container").hide(); 
