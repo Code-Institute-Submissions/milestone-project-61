fc52f9a27b1ce0edce18ed74c7a2e4c9e62be496
mvmolloy <mvamolloy@gmail.com>
2020-05-21 19:49:06 +0000

    adding some comments to jquery

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
    getWeatherData();
 })
 
+//function calls weather data from OpenWeather API
 function getWeatherData() {
     var xhr = new XMLHttpRequest();
     //url gets data for aviemore coordinates, api key, and then the "metric" pulls them in celcius rather than ke
     var url = "https://api.openweathermap.org/data/2.5/weather?lat=57.06&lon=-3.6061&appid=422a169bc688adda6305539c7e845ffd&units=metric"
-  //var weatherJSON = [];
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let weatherResponse = (JSON.parse(this.responseText));
@@ -17,18 +17,18 @@ function getWeatherData() {
     xhr.open("GET", url);
     xhr.send();
 } 
-
+//preparing weather response data 
 function setWeatherData(weatherResponse) {
     let tempRounded = Math.round(weatherResponse.main.temp, 1)
     let weatherIconCode = weatherResponse.weather[0].icon 
-    //uncomment below line to test different weather codes
+    //comment above line and uncomment below line to test different weather codes
         //let weatherIconCode = "01n" 
         //01d = clear; 02d, 03d, 04d = clouds; 09d, 10d = rain; 11d = thunderstorm; 13d = snow; 50d = mist;  
     let weatherIconURL =  `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
     setModalGif(weatherIconCode);
     setModalWeather(tempRounded, weatherIconCode, weatherIconURL);
 } 
-
+//use weather response data prepared above to set the modal gif and content
 function setModalGif(weatherIconCode) {
     let weatherIconTrunc = weatherIconCode.substring(0, 2);
     if (weatherIconCode.charAt(2) == "d") {  
@@ -67,6 +67,7 @@ function setModalGif(weatherIconCode) {
         $("#call-text").html("Call Tomorrow");
     }
 } 
+//use weather response data prepared above to display weather icon and temperature in modal
 
 function setModalWeather(tempRounded, weatherIconCode, weatherIconURL) {
     if (weatherIconCode.charAt(2) == "d") {
